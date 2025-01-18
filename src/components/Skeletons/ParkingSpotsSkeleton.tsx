285cd3a7733284eeb64438789a343edfcca2ad9c
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2025-01-18 22:16:31 +0900

    style: minor css fix

--- src/components/Skeletons/ParkingSpotsSkeleton.tsx
+++ src/components/Skeletons/ParkingSpotsSkeleton.tsx
@@ -3,7 +3,7 @@ function ParkingSpotsSkeleton() {
     <div className="grid grid-cols-3 gap-4">
       {Array.from({ length: 9 }).map((_, index) => (
         <div className="animate-pulse" key={index}>
-          <div className="h-[150px] min-w-[100px] bg-gray-200 rounded-lg" />
+          <div className="min-h-[150px] min-w-[150px] bg-gray-200 rounded-lg" />
         </div>
       ))}
     </div>
